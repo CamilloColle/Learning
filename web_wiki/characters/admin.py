57f1a990810c7125a4cd346c78f4a2661feb7f77
CamilloColle <camillo.colleluori@gmail.com>
2025-03-19 19:49:11 +0100

    implemented Character class extensions PC and NPC

--- web_wiki/characters/admin.py
+++ web_wiki/characters/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Character
+from .models import Character, PlayerCharacter, NonPlayerCharacter
 
 admin.site.register(Character)
+admin.site.register(PlayerCharacter)
+admin.site.register(NonPlayerCharacter)
+
