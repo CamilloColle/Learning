a4964237debb8e38648902e72e73cfba076763a3
CamilloColle <camillo.colleluori@gmail.com>
2025-03-20 20:15:37 +0100

    implemented template for specific campaign pages and respective url and views configs

--- web_wiki/campaigns/urls.py
+++ web_wiki/campaigns/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'campaigns' #specify that these urls are in the app 'campaigns'
+
 urlpatterns = [
-    path('', views.campaigns_list),
+    path('', views.campaigns_list, 
+         name='list'), #name for the link
+    path('<slug:slug>', views.campaign_page, 
+         name='page'), 
 ]
--- web_wiki/campaigns/views.py
+++ web_wiki/campaigns/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from .models import Campaign
+#from django.http import HttpResponse
 
 def campaigns_list(request):
     campaigns = Campaign.objects.all().order_by('start_date')
     return render(request, 'campaigns_home.html',  #no need to specify template because set in settings
                   { 'campaigns': campaigns}) # return data relative to the Campaign objects
+
+
+def campaign_page(request, slug):
+    campaign = Campaign.objects.get(slug=slug)
+    return render(request, 'campaign_page.html',  #render specific page
+                  { 'campaign': campaign}) # return data relative to the Campaign objects
 
 
