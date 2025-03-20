7f9588386b83ded4773b6d38e686dc3c370e438c
CamilloColle <camillo.colleluori@gmail.com>
2025-03-20 16:32:44 +0100

    updated campaigns views to display basic campaign information

--- web_wiki/campaigns/views.py
+++ web_wiki/campaigns/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Campaign
 
 def campaigns_list(request):
-    return render(request, 'campaigns_home.html') #no need to specify template because set in settings
+    campaigns = Campaign.objects.all().order_by('start_date')
+    return render(request, 'campaigns_home.html',  #no need to specify template because set in settings
+                  { 'campaigns': campaigns}) # return data relative to the Campaign objects
 
 
